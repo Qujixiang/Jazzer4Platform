a1a01ca3c65fc3eff858d676a791575f1cb5c576
feixiangdejiahao <714634079@qq.com>
2024-03-04 15:19:21 +0800

    update

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -334,12 +334,12 @@ public class Client {
 
     private void checkMaven(Task task) {
         File taskFile = new File(task.targetProgramPath);
-        Map<String, String> env = new HashMap<>();
-        env.put("maven.multiModuleProjectDirectory", taskFile.getAbsolutePath());
-
+        String cmd = "mvn clean package";
         try {
-            Verifier v = new Verifier(taskFile.getAbsolutePath());
-            v.executeGoals(Arrays.asList("clean", "package"), env);
+            Process process = Runtime.getRuntime().exec(cmd, null, taskFile);
+            while (process.isAlive()) {
+                Thread.sleep(100);
+            }
             System.out.println("构建成功！");
             File file = new File(task.targetProgramPath + "target/");
             File[] files = file.listFiles();
